4730548cdcce3ad65f2f869edd55275afacaed51
levintoo <tooklevin@gmail.com>
2021-06-06 13:34:02 +0300

    random black circles now moving

--- canvas.js
+++ canvas.js
@@ -34,37 +34,59 @@ c.stroke();
 } */
 //arc/cirle
 
-function Circle(x, y) {
+
+function Circle(x, y, dx, dy, radius) {
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
 
     this.draw =function(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = 'blue'
+        c.stroke();
+        c.fill();
 
+    }
+    this.update = function(){
+            if (this.x + this.radius > innerWidth || 
+                this.x - this.radius < 0) {
+                this.dx = -this.dx;
+            }
+            if (this.y + this.radius > innerHeight ||
+                 this.y - this.radius < 0) {
+                this.dy = -this.dy;
+            }
+            this.x += this.dx;
+            this.y += this.dy;
+            this.draw();
     }
 }
 
 
-var x =Math.random() *  innerWidth;
-var y =Math.random() *  innerHeight;
-var dx =(Math.random() -0.5)* 5;
-var dy =(Math.random() -0.5) * 5;
-var radius = 30;
+var circleArray = [];
+
+for (var i = 0; i < 500; i++){
+    var x =Math.random() *  (innerWidth - radius * 2) +radius;
+    var y =Math.random() *  (innerHeight - radius * 2) +radius;
+    var dx =(Math.random() -0.5)* 5;
+    var dy =(Math.random() -0.5) * 5;
+    var radius = 30;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+    var circle = new Circle(200, 200, 3, 3, 30 );
+
+}
+
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue'
-    c.stroke();
-    
-if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
-}
-if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-}
-    x += dx;
-    y += dy;
-}
+   for (var i = 0; i < circleArray.length; i++)
+   circleArray[i] .update();
+ }
+  
 animate();
